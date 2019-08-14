4068e594315274ab081c36e40c26eddd1c8bd54f
SamyakSahu <47889349+SamyakSahu@users.noreply.github.com>
2019-08-14 18:54:36 +0530

    Added hardware and software interrupts for encoder.
    Probably won't require much change after this. Check for the Serial.begin() baud rate.

--- SETUP/SETUP.ino
+++ SETUP/SETUP.ino
@@ -1,8 +1,9 @@
-#include <MedianFilter.h>
+#include <PinChangeInterrupt.h>
 #include <SharpDistSensor.h>
-
-#define lf 2
-#define lb 3
+//Reserve 2 and 3 for encoders (also add 11 and 12)
+#define pi 3.14159
+#define lf 7
+#define lb 8
 #define rf 5
 #define rb 4
 #define re 9
@@ -10,16 +11,15 @@
 #define FS A0
 #define RS A1
 #define LS A2
-#define eRA 7   //encoder variables; PWM not required
-#define eRB 8
-#define eLA 10
-#define eLB 11
+#define encoderRF 2
+#define encoderRB 3
+#define encoderLF 12
+#define encoderLB 11
 SharpDistSensor frontSensor(FS, 1);
 SharpDistSensor rightSensor(RS, 1);
 SharpDistSensor leftSensor(LS, 1);
-
-volatile unsigned long int lCount = 0;
-volatile unsigned long int rCount = 0;
+volatile unsigned long int encoder_R = 0;
+volatile unsigned long int encoder_L = 0;
 
 /*
  * MICROMOUSE CODE!
@@ -41,14 +41,12 @@ void setup(){
   pinMode(LS, INPUT);
   pinMode(RS, INPUT);
   pinMode(FS, INPUT);
-  pinMode(eRA, INPUT);
-  pinMode(eRB, INPUT);
-  pinMode(eLA, INPUT);
-  pinMode(eLB, INPUT);
+  pinMode(encoderRF, INPUT_PULLUP);
+  pinMode(encoderLF, INPUT_PULLUP);
+  attachInterrupt(digitalPinToInterrupt(encoderRF), encoderR, CHANGE);                   //experiment with
+  attachPinChangeInterrupt(digitalPinToPinChangeInterrupt(encoderLF), encoderL, CHANGE);  //RISING, FALLING and CHANGE parameters.
   frontSensor.setModel(SharpDistSensor::GP2Y0A41SK0F_5V_DS);
   rightSensor.setModel(SharpDistSensor::GP2Y0A41SK0F_5V_DS);
   leftSensor.setModel(SharpDistSensor::GP2Y0A41SK0F_5V_DS);
-  Serial.begin(9600);
-  attachInterrupt(0, lCount, CHANGE);
-  attachInterrupt(1, rCount, CHANGE);
+  Serial.begin(112500);
   }
